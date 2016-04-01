a594109ef9fd20a861508fd759d771d50dec4fd6
Ansuraj Khadanga <ansurajk@Ansurajs-MacBook-Air.local>
2016-04-01 12:08:13 +0530

    first java commit

---
+++ src/practice/ThreadEx.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class ThreadEx{
+	
+	public static void main(String args[]) {
+		System.out.println("Hi, Welcome!");
+		
+		Threads t = new Threads();
+		t.start();
+		
+	}
+}
+
+
---
+++ src/practice/Threads.java
@@ -0,0 +1,37 @@
+package practice;
+
+public class Threads implements Runnable  {
+
+	private Thread t1, t2;
+	
+	private int count = 0;
+
+	public void run() {
+		
+		System.out.println("started");
+		
+		while(this.count < 10 ) {
+			try {
+				System.out.println(this.count + "--------" + Thread.currentThread().getName());
+				
+				Thread.sleep(500);
+				this.count++;
+				} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				}
+		}
+		
+		System.out.println("ended");
+		
+		return;
+	}
+	
+	public void start() {
+		t1 = new Thread(this, "t1 thread");
+		t1.start();
+		
+		t2 = new Thread(this, "t2 thread");
+		t2.start();
+	}
+}
